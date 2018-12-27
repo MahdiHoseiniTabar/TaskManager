9881d677da651f5e696b34ac590ae06a3b29a0c8
mahdi_Hoseini_Tabar <mht0440232@gmail.com>
2018-12-27 14:56:12 +0330

    Totality Create ListActivity

--- app/src/main/java/com/example/caspian/taskmanager/ListActivity.java
+++ app/src/main/java/com/example/caspian/taskmanager/ListActivity.java
@@ -1,23 +1,73 @@
 package com.example.caspian.taskmanager;
 
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.caspian.taskmanager.model.Task;
+import com.example.caspian.taskmanager.model.TaskLab;
+
+import java.util.List;
+
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ListActivity extends AppCompatActivity {
-
+    private TabLayout mTabLayout;
+    private ViewPager mViewPager;
+    private FloatingActionButton mFloatingActionButton;
+    private Task mTask;
+    private TaskLab mTaskLab;
+    private List<Task> mTaskList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
+        mTaskList = TaskLab.getmInstance().getTaskList();
 
+
+        mFloatingActionButton = findViewById(R.id.floatingActionButton);
+        mFloatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        mTabLayout = findViewById(R.id.tab_layout);
+
+        mViewPager = findViewById(R.id.view_pager);
+        mViewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return null;
+            }
+
+            @Override
+            public int getCount() {
+                return 2;
+            }
+
+            @Nullable
+            @Override
+            public CharSequence getPageTitle(int position) {
+                switch (position){
+                    case 0:
+                        return "All";
+                    case 1:
+                    return "Done";
+                    default:
+                        return "";
+                }
+            }
+        });
+        mTabLayout.setupWithViewPager(mViewPager);
 
     }
 }

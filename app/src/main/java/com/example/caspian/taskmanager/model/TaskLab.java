7a5bafec7d4f6e31543461d73f8d9bc6d357870e
mahdi_Hoseini_Tabar <mht0440232@gmail.com>
2018-12-27 11:53:15 +0330

    Create SingleTone TaskLab

--- app/src/main/java/com/example/caspian/taskmanager/model/TaskLab.java
+++ app/src/main/java/com/example/caspian/taskmanager/model/TaskLab.java
@@ -1,4 +1,53 @@
 package com.example.caspian.taskmanager.model;
 
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.UUID;
+
+//singleton Repository
 public class TaskLab {
+    private static TaskLab mInstance;
+    private static List<Task> mTaskList = new ArrayList<>();
+    private static HashMap<UUID,Task> mHashMap = new LinkedHashMap<>();
+
+    public static TaskLab getmInstance() {
+        if (mInstance == null)
+            mInstance =  new TaskLab();
+        return mInstance;
+    }
+
+    private TaskLab(){
+    }
+
+    public void addTask(String title, String description, Date date, boolean done){
+        Task task = new Task();
+        task.setTitle(title);
+        task.setDescribtion(description);
+        task.setDate(date);
+        task.setDone(done);
+        mTaskList.add(task);
+        mHashMap.put(task.getId(),task);
+    }
+
+    public static List<Task> getTaskList(){
+        return mTaskList;
+    }
+
+    public List<Task> getDoneTasks(){
+        List<Task> doneTasks = new ArrayList<>();
+        for (int i = 0; i < mTaskList.size() ; i++) {
+            if (mTaskList.get(i).isDone())
+                doneTasks.add(mTaskList.get(i));
+        }
+        return doneTasks;
+    }
+
+    public Task getTask(UUID id){
+        return mHashMap.get(id);
+    }
 }

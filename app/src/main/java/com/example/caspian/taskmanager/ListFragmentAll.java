4fac4105c6bcfb405cfb8d6088d8143ec5c9843c
mahdi_Hoseini_Tabar <mht0440232@gmail.com>
2018-12-27 14:41:33 +0330

    Set Adapters of ListFragmentAll

--- app/src/main/java/com/example/caspian/taskmanager/ListFragmentAll.java
+++ app/src/main/java/com/example/caspian/taskmanager/ListFragmentAll.java
@@ -2,16 +2,24 @@ package com.example.caspian.taskmanager;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ListFragmentAll extends Fragment {
+    private RecyclerView mRecyclerView;
+    private AllTaskAdapter allTaskAdapter;
 
 
     public ListFragmentAll() {
@@ -22,8 +30,54 @@ public class ListFragmentAll extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list_all, container, false);
+        View view = inflater.inflate(R.layout.fragment_list_all,container ,false);
+
+        mRecyclerView = view.findViewById(R.id.recycler_all);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        if (allTaskAdapter == null)
+             allTaskAdapter = new AllTaskAdapter();
+        mRecyclerView.setAdapter(allTaskAdapter);
+
+        return view;
+    }
+
+    public class AllTaskAdapter extends RecyclerView.Adapter<AllTaskAdapter.TaskHolder>{
+
+        @NonNull
+        @Override
+        public TaskHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            View view = layoutInflater.inflate(R.layout.item_list_all, parent ,false);
+            TaskHolder taskHolder = new TaskHolder(view);
+            return taskHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull TaskHolder holder, int position) {
+
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
+
+        public class TaskHolder extends RecyclerView.ViewHolder{
+            private CircleImageView mCircleImageView;
+            private TextView title;
+            private TextView date;
+
+
+            public TaskHolder(View itemView) {
+                super(itemView);
+
+                mCircleImageView = itemView.findViewById(R.id.item_list_all_circle_image);
+                title = itemView.findViewById(R.id.item_list_all_title);
+                date = itemView.findViewById(R.id.item_list_all_date);
+            }
+        }
     }
 
 }

6a2a7a2d8ce58c01af93a19ba091583905ad13d4
mahdi_Hoseini_Tabar <mht0440232@gmail.com>
2018-12-27 14:00:22 +0330

    Adding ListFragmentDone

--- app/src/main/java/com/example/caspian/taskmanager/ListFragmentAll.java
+++ app/src/main/java/com/example/caspian/taskmanager/ListFragmentAll.java
@@ -11,10 +11,10 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ListFragment extends Fragment {
+public class ListFragmentAll extends Fragment {
 
 
-    public ListFragment() {
+    public ListFragmentAll() {
         // Required empty public constructor
     }
 
@@ -23,7 +23,7 @@ public class ListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false);
+        return inflater.inflate(R.layout.fragment_list_all, container, false);
     }
 
 }
